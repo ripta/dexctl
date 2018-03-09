31f48d830a081f074604a62e4a239d1830cdd1e2
Ripta Pasay <ripta@users.noreply.github.com>
2018-03-09 15:39:37 -0800

    dexctl: make parameters configurable

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/coreos/dex/api"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+)
+
+var (
+	dexCAPath = flag.String("ca-cert", "/etc/dex/grpc.crt", "Path to CA certificate")
+	dexHost   = flag.String("dex-host", "127.0.0.1:5557", "The host:port to dex's gRPC port")
 )
 
 func newDexClient(hostAndPort, caPath string) (api.DexClient, error) {
@@ -18,27 +24,37 @@ func newDexClient(hostAndPort, caPath string) (api.DexClient, error) {
 
 	conn, err := grpc.Dial(hostAndPort, grpc.WithTransportCredentials(creds))
 	if err != nil {
-		return nil, fmt.Errorf("dail: %v", err)
+		return nil, fmt.Errorf("dial: %v", err)
 	}
 	return api.NewDexClient(conn), nil
 }
 
 func main() {
-	client, err := newDexClient("127.0.0.1:5557", "/etc/dex/grpc.crt")
+	flag.Parse()
+
+	client, err := newDexClient(*dexHost, *dexCAPath)
 	if err != nil {
 		log.Fatalf("failed creating dex client: %v ", err)
 	}
 
-	req := &api.CreateClientReq{
-		Client: &api.Client{
-			Id:           "example-app",
-			Name:         "Example App",
-			Secret:       "ZXhhbXBsZS1hcHAtc2VjcmV0",
-			RedirectUris: []string{"http://127.0.0.1:5555/callback"},
-		},
+	req := &api.VersionReq{}
+	rsp, err := client.GetVersion(context.Background(), req)
+	if err != nil {
+		log.Fatalf("failed querying for version: %v", err)
 	}
 
-	if _, err := client.CreateClient(context.TODO(), req); err != nil {
-		log.Fatalf("failed creating oauth2 client: %v", err)
-	}
+	log.Printf("got version: %+v", rsp)
+
+	// req := &api.CreateClientReq{
+	// 	Client: &api.Client{
+	// 		Id:           "example-app",
+	// 		Name:         "Example App",
+	// 		Secret:       "ZXhhbXBsZS1hcHAtc2VjcmV0",
+	// 		RedirectUris: []string{"http://127.0.0.1:5555/callback"},
+	// 	},
+	// }
+
+	// if _, err := client.CreateClient(context.TODO(), req); err != nil {
+	// 	log.Fatalf("failed creating oauth2 client: %v", err)
+	// }
 }
